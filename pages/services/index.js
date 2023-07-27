7595b7e05ed661862e727ee38a5c3e1626a9fe37
anggaihza <angga.asep98@gmail.com>
2023-07-27 14:16:35 +0700

    aa

--- pages/services/index.js
+++ pages/services/index.js
@@ -20,7 +20,7 @@ const Services = () => {
               animate="show"
               exit="hidden"
               className="h2 xl:mt-8">
-              My services <span className="text-accent">&quot;.&quot;</span>
+              My services <span className="text-accent">.</span>
             </motion.h2>
             <motion.p
               variants={fadeIn("up", 0.4)}
